e9937dbd0210382f2dc626cb037d3cc41fd3ce53
mashaojie <mashaojie@pinefield-inc.com>
2020-11-04 12:52:33 +0800

    规范网站

--- js/main.js
+++ js/main.js
@@ -2,23 +2,21 @@ $(document).ready(function() {
 
 
   $('a.blog-button').click(function() {
+    $('.beian-class').css('display', "none");
     // If already in blog, return early without animate overlay panel again.
     if (location.hash && location.hash == "#blog") { 
-      $('.beian-class').css('display', "none");
       return
     };
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) { 
-      $('.beian-class').css('display', "none");
       return
     };
     $('.main-post-list').removeClass('hidden');
     currentWidth = $('.panel-cover').width();
     if (currentWidth < 2000) {
       $('.panel-cover').addClass('panel-cover--collapsed');
-      $('.beian-class').css('display', "none");
     } else {
-      $('.panel-cover').css('max-width',currentWidth);
-      $('.panel-cover').animate({'max-width': '320px', 'width': '22%'}, 400, swing = 'swing', function() {} );
+      // $('.panel-cover').css('max-width',currentWidth);
+      $('.panel-cover').animate({'width': '17%'}, 400, swing = 'swing', function() {} );
     }
 
     
@@ -26,13 +24,11 @@ $(document).ready(function() {
 
   if (window.location.hash && window.location.hash == "#blog") {
     $('.panel-cover').addClass('panel-cover--collapsed');
-    $('.beian-class').css('display', "none");
     $('.main-post-list').removeClass('hidden');
   }
 
   if (window.location.pathname.substring(0, 5) == "/tag/") {
     $('.panel-cover').addClass('panel-cover--collapsed');
-    $('.beian-class').css('display', "none");
   }
 
   $('.btn-mobile-menu__icon').click(function() {
